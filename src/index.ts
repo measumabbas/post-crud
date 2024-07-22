373e4e83ad0715570a16dbe16f615a9b3d795c93
Measum Abbas <maisumabbas.charbu@gmail.com>
2024-07-22 13:39:13 +0500

    cors-added

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,12 @@ import express, { Request, Response } from "express";
 import dotenv from "dotenv";
 import { connectToDb } from "./utils/connectToDb";
 import postRoutes from './routes/postRoutes'
+import cors from 'cors'
 dotenv.config();
 
 const app = express();
-app.use(express.json())
+app.use(express.json());
+app.use(cors())
 app.use('/api/v1',postRoutes)
 app
   .listen(process.env.PORT, () => {
